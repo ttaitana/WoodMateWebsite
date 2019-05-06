b9b9d46525309be7594da8d76b500be511087bfd
tt_umv <60070029@it.kmitl.ac.th>
2019-05-06 18:18:37 +0700

    style profile page

--- backend/woodmate/shop/forms.py
+++ backend/woodmate/shop/forms.py
@@ -4,38 +4,58 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth import authenticate
 from shop.models import *
 
+
 class CustomerForm(forms.Form):
     first_name = forms.CharField(max_length=50, required=True)
     last_name = forms.CharField(max_length=50, required=True)
     phone_number = forms.CharField(max_length=10, required=True)
 
-    first_name.widget.attrs.update({'class': 'form-control', 'placeholder': 'First Name'})
-    last_name.widget.attrs.update({'class': 'form-control', 'placeholder': 'Last Name'})
-    phone_number.widget.attrs.update({'class': 'form-control', 'placeholder': 'eg. 089-147-xxxx'})
+    first_name.widget.attrs.update(
+        {'class': 'form-control', 'placeholder': 'First Name'})
+    last_name.widget.attrs.update(
+        {'class': 'form-control', 'placeholder': 'Last Name'})
+    phone_number.widget.attrs.update(
+        {'class': 'form-control', 'placeholder': 'eg. 089-147-xxxx'})
+
 
 class AddressForm(forms.Form):
-    address_desc = forms.CharField(max_length=500, required=True, widget=forms.Textarea)
+    # address_desc = forms.CharField(max_length=500, required=True, widget=forms.Textarea(attrs={'class': 'md-textarea form-control'}))
+    address_desc = forms.CharField(max_length=500, required=True)
     district = forms.CharField(max_length=50, required=True)
     area = forms.CharField(max_length=50, required=True)
     province = forms.CharField(max_length=50, required=True)
     postal_code = forms.CharField(max_length=5, required=True)
 
+    address_desc.widget.attrs.update({'class': 'form-control'})
+    district.widget.attrs.update({'class': 'form-control'})
+    area.widget.attrs.update({'class': 'form-control'})
+    province.widget.attrs.update({'class': 'form-control'})
+    postal_code.widget.attrs.update({'class': 'form-control'})
+
+
 def validate_passlen(value):
     if len(value) < 8:
         raise ValidationError('รหัสผ่าน ต้องมีความยาวอย่างน้อย 8 ตัวอักษร')
 
+
 class UserForm(forms.Form):
-    username = forms.CharField(label='ชื่อผู้ใช้', max_length=100, required=True)
-    password = forms.CharField(label='รหัสผ่าน', max_length=100, required=True, validators=[validate_passlen], widget=forms.PasswordInput)
-    confirmpass = forms.CharField(label='ยืนยันรหัสผ่าน', max_length=100, required=True, widget=forms.PasswordInput)
+    username = forms.CharField(
+        label='ชื่อผู้ใช้', max_length=100, required=True)
+    password = forms.CharField(label='รหัสผ่าน', max_length=100, required=True, validators=[
+                               validate_passlen], widget=forms.PasswordInput)
+    confirmpass = forms.CharField(
+        label='ยืนยันรหัสผ่าน', max_length=100, required=True, widget=forms.PasswordInput)
     email = forms.EmailField(required=True)
 
     email.widget.attrs.update({'class': "form-control",
-                    'aria-describedby': "emailHelp",
-                    'placeholder':"eg.woodmate@furniture.com"})
-    password.widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
-    confirmpass.widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm Password'})
-    username.widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
+                               'aria-describedby': "emailHelp",
+                               'placeholder': "eg.woodmate@furniture.com"})
+    password.widget.attrs.update(
+        {'class': 'form-control', 'placeholder': 'Password'})
+    confirmpass.widget.attrs.update(
+        {'class': 'form-control', 'placeholder': 'Confirm Password'})
+    username.widget.attrs.update(
+        {'class': 'form-control', 'placeholder': 'Username'})
 
     def clean(self):
         cleaned_data = super().clean()
@@ -44,8 +64,14 @@ class UserForm(forms.Form):
         if password != confirmpass:
             raise ValidationError('รหัสผ่าน และ ยืนยันรหัสผ่าน ต้องเหมือนกัน')
 
+
 class FeedbackForm(forms.Form):
-    text = forms.CharField(max_length=500)
+    text = forms.CharField(max_length=500, widget=forms.Textarea)
+
+    text.widget.attrs.update({
+        'class': 'form-control'
+    })
+
 
 class MakeOrderForm(forms.Form):
     payment = forms.CharField(max_length=50)
